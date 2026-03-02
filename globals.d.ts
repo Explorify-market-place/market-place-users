c1d89b622c562becf1f3f098b77b22e9ff7288a1
Suryansh Dey <deysuryansh@gmail.com>
2026-03-02 12:39:39 +0530

    Adding trip details page

--- globals.d.ts
+++ globals.d.ts
@@ -1,4 +1,4 @@
-declare module "*.css" {
-  const content: { [className: string]: string };
-  export default content;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Window {
+  google: any;
 }
